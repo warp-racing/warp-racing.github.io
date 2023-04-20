dcc70809bf4e160f6cf5b6123671f4d68554eb33
skifli <121291719+skifli@users.noreply.github.com>
2023-04-20 15:27:26 +0000

    feat: add all pages

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("welcome").addEventListener("mousemove", function parallax(event) {
-    let _w = window.innerWidth / 2;
-    let _h = window.innerHeight / 2;
-    let _mouseX = event.clientX;
-    let _mouseY = event.clientY;
-
-    this.style.backgroundPosition = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}% , ${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}% , ${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-});
