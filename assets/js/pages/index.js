a16c3c4ce4fb4a1c9f384aff124b9923075bf623
skifli <121291719+skifli@users.noreply.github.com>
2023-09-23 15:13:59 +0000

    feat: woo even better

--- assets/js/pages/index.js
+++
@@ -1,9 +0,0 @@
-import { Application } from "https://cdn.skypack.dev/@splinetool/runtime@0.9.455";
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/aCxuV9kKR2s7IVPh/scene.splinecode').then(
-    () => {
-
-    }
-);
