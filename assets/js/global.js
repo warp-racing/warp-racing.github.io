7113c6f6e1cfac853a0461ae6eb13fb9161402ef
skifli <121291719+skifli@users.noreply.github.com>
2023-09-25 19:23:30 +0000

    fixL cursor being laggy

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,13 +1,12 @@
+const cursorFollower = document.getElementById("cursor-follower");
+
 document.addEventListener("mousemove", (e) => {
-    const cursorFollower = document.getElementById("cursor-follower");
     const lag = 20; // Adjust the lag value to your preference
 
     // Calculate the new position with a bit of lag
     const newX = e.clientX - lag / 2;
     const newY = e.clientY - lag / 2;
 
-    cursor.style.transform = `translate(${e.clientX - 1 + window.scrollX}px, ${e.clientY - 1 + window.scrollY}px)`;
-
     // Apply the new position to the cursor follower
-    cursorFollower.style.transform = `translate(${newX + window.scrollX}px, ${newY + window.scrollY}px)`;
+    cursorFollower.style.transform = `translate(${newX + window.scrollX + 1}px, ${newY + window.scrollY + 1}px)`;
 });
