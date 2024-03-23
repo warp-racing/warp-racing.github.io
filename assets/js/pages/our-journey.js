cbb8b0af3e057d973b965c8626de60ab79d3d8f8
skifli <121291719+skifli@users.noreply.github.com>
2024-03-23 17:21:55 +0000

    feat: add nationals!

--- assets/js/pages/our-journey.js
+++ assets/js/pages/our-journey.js
@@ -1,11 +1,14 @@
 let entryClassCarImage = document.getElementById('entry-class-car-image');
 let devClassCarImage = document.getElementById('dev-class-car-image');
+let devClassCarImageNats = document.getElementById('dev-class-car-image-nats');
 
 let imageCarouselDevClass2024_LondonSouthRegionals = document.getElementById('image-carousel-dev-class-2024_london-south-regionals');
+let imageCarouselDevClass2024_UKNationals = document.getElementById('image-carousel-dev-class-2024_uk-nationals');
 
 var carSlide = 0;
 
 const imageCarouselDevClass2024_LondonSouthRegionalsImages = 6;
+const imageCarouselDevClass2024_UKNationalsImages = 11;
 
 function moveCarousel_DevClass2024_LondonSouthRegionals() {
     let carouselFirstItem = imageCarouselDevClass2024_LondonSouthRegionals.firstElementChild;
@@ -42,6 +45,41 @@ function moveCarousel_DevClass2024_LondonSouthRegionals() {
     }, 5000);
 }
 
+async function moveCarousel_DevClass2024_UKNationals() {
+    let carouselFirstItem = imageCarouselDevClass2024_UKNationals.firstElementChild;
+    let firstItemWidth = carouselFirstItem.firstElementChild.width;
+
+    /* move all items to the left */
+
+    for (let i = 0; i < imageCarouselDevClass2024_UKNationals.childElementCount; i++) {
+        let item = imageCarouselDevClass2024_UKNationals.children[i];
+
+        item.style.transform = `translateX(-${firstItemWidth}px)`;
+        item.style.transition = `transform 5s ease-in-out`;
+    }
+
+    setTimeout(function () {
+        for (let i = 0; i < imageCarouselDevClass2024_UKNationals.childElementCount; i++) {
+            let item = imageCarouselDevClass2024_UKNationals.children[i];
+
+            item.style.transform = `translateX(0)`;
+            item.style.transition = `none`;
+        }
+
+        /* add first item to the end */
+
+        let copiedFirstItem = carouselFirstItem.cloneNode(true);
+        copiedFirstItem.onclick = function () {
+            window.open(`../assets/img/our-journey/dev-class-2024_uk-nationals/${carouselFirstItem.dataset.image}.jpg`, '_blank');
+        }
+        imageCarouselDevClass2024_UKNationals.appendChild(copiedFirstItem);
+
+        imageCarouselDevClass2024_UKNationals.removeChild(carouselFirstItem);
+
+        moveCarousel_DevClass2024_UKNationals();
+    }, 5000);
+}
+
 async function setupCarousel_DevClass2024_LondonSouthRegionals() {
     for (let i = 2; i <= imageCarouselDevClass2024_LondonSouthRegionalsImages; i++) { /* we start from 2 and have 1 manually inputted because sometimes the browser glitches and returns 0 widths for newly appended elements. could never find a work around :`( */
         let div = document.createElement('div');
@@ -65,9 +103,32 @@ async function setupCarousel_DevClass2024_LondonSouthRegionals() {
         item.dataset.image = i+1;
     }
 
-    await new Promise(r => setTimeout(r, 5000));
-    
-    moveCarousel_DevClass2024_LondonSouthRegionals();
+    setTimeout(moveCarousel_DevClass2024_LondonSouthRegionals, 5000);
+}
+
+async function setupCarousel_DevClass2024_UKNationals() {
+    for (let i = 2; i <= imageCarouselDevClass2024_UKNationalsImages; i++) { /* we start from 2 and have 1 manually inputted because sometimes the browser glitches and returns 0 widths for newly appended elements. could never find a work around :`( */
+        let div = document.createElement('div');
+
+        div.classList.add('carousel-item');
+        
+        let img = document.createElement('img');
+        img.src = `../assets/img/our-journey/dev-class-2024_uk-nationals/${i}-small.webp`;
+        img.alt = `Dev Class 2024 UK Nationals ${i}`;
+        
+        div.appendChild(img);
+        imageCarouselDevClass2024_UKNationals.appendChild(div);
+    }
+
+    for (let i = 0; i < imageCarouselDevClass2024_UKNationals.childElementCount; i++) {
+        let item = imageCarouselDevClass2024_UKNationals.children[i];
+
+        item.onclick = function () {
+            window.open(`../assets/img/our-journey/dev-class-2024_uk-nationals/${i + 1}.jpg`, '_blank');
+        }
+    }
+
+    setTimeout(moveCarousel_DevClass2024_UKNationals, 5000);
 }
 
 function changeCar() {
@@ -77,6 +138,7 @@ function changeCar() {
 
     entryClassCarImage.style["background-image"] = `url(../assets/img/entry-class-car/${carSlide}.webp)`;
     devClassCarImage.style["background-image"] = `url(../assets/img/dev-class-car/${carSlide}.webp)`;
+    devClassCarImageNats.style["background-image"] = `url(../assets/img/dev-class-car/${carSlide}-nats.webp)`;
 
     setTimeout(changeCar, 5000);
 }
@@ -87,22 +149,30 @@ function preloadImage(url) {
     img.src = url;
 }
 
-function preloadImages() {
+async function preloadImages() {
     for (let i = 1; i <= 4; i++) {
         preloadImage(`../assets/img/entry-class-car/${i}.webp`);
         preloadImage(`../assets/img/dev-class-car/${i}.webp`);
+        preloadImage(`../assets/img/dev-class-car/${i}-nats.webp`);
 
         preloadImage(`../assets/img/our-journey/dev-class-2024_london-south-regionals/${i}-small.webp`);
+        preloadImage(`../assets/img/our-journey/dev-class-2024_uk-nationals/${i}-small.webp`);
     }
 
     for (let i = 5; i <= imageCarouselDevClass2024_LondonSouthRegionalsImages; i++) {
         preloadImage(`../assets/img/our-journey/dev-class-2024_london-south-regionals/${i}-small.webp`);
+        preloadImage(`../assets/img/our-journey/dev-class-2024_uk-nationals/${i}-small.webp`);
+    }
+
+    for (let i = 7; i <= imageCarouselDevClass2024_UKNationalsImages; i++) {
+        preloadImage(`../assets/img/our-journey/dev-class-2024_uk-nationals/${i}-small.webp`);
     }
 }
 
-window.addEventListener("load", function () {
-    preloadImages();
+window.addEventListener("load", async function () {
+    await preloadImages();
 
     setupCarousel_DevClass2024_LondonSouthRegionals();
+    setupCarousel_DevClass2024_UKNationals();
     changeCar();
 });
